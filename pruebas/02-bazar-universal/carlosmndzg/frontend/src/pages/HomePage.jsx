31335c86fff573889bd5e0513befbf41942c15d9
carlosmndzg <cjr2344@gmail.com>
2023-09-18 19:11:19 +0200

    Changed button search homepage

--- pruebas/02-bazar-universal/carlosmndzg/frontend/src/pages/HomePage.jsx
+++ pruebas/02-bazar-universal/carlosmndzg/frontend/src/pages/HomePage.jsx
@@ -18,20 +18,20 @@ function HomePage() {
 
   return (
     <div className="h-[100dvh] bg-stone-50 px-12 text-stone-950">
-      <div className="mx-auto flex h-full max-w-sm flex-col items-center justify-center">
-        <h1 className="mb-6 text-center text-4xl font-bold">Bazar Online</h1>
-        <form onSubmit={handleForm} className="flex flex-col">
+      <div className="mx-auto flex h-full max-w-xl flex-col items-center justify-center">
+        <h1 className="mb-24 text-center text-5xl font-bold">Bazar Online</h1>
+        <form onSubmit={handleForm} className="relative flex self-stretch">
           <input
             value={search}
             onChange={e => setSearch(e.target.value)}
             type="text"
-            className="mb-6 rounded-xl bg-stone-200 px-8 py-4 text-xl focus:outline-none focus:ring focus:ring-stone-400"
+            className="w-full rounded-xl bg-stone-200 px-8 py-4 text-xl transition-[box-shadow] duration-300 focus:outline-none focus:ring focus:ring-stone-400"
             placeholder="Laptops, smartphones..."
           />
           <input
             type="submit"
             value="Buscar"
-            className="cursor-pointer self-center rounded-full bg-pink-300 px-16 py-3 text-2xl font-semibold text-pink-950 hover:bg-pink-400 focus:outline-none focus:ring focus:ring-pink-500"
+            className="absolute right-0 top-0 h-full cursor-pointer self-center rounded-r-xl bg-pink-300 px-8 py-3 text-2xl font-semibold text-pink-950 transition-colors hover:bg-pink-400 focus:outline-none focus:ring focus:ring-pink-500"
           />
         </form>
       </div>
