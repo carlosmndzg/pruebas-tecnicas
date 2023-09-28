5cfa64bee74c2b5d9c890c2f3b454afb610bf2d0
carlosmndzg <cjr2344@gmail.com>
2023-09-28 08:34:47 +0200

    Added 404 page, improved items page and added rating component

--- pruebas/02-bazar-universal/carlosmndzg/frontend/src/layout/Layout.jsx
+++ pruebas/02-bazar-universal/carlosmndzg/frontend/src/layout/Layout.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
-import { Link, Outlet, useNavigate } from 'react-router-dom'
+import { Link, Outlet, useNavigate, useNavigation } from 'react-router-dom'
+import Spinner from '../components/Spinner'
 
 function Layout() {
   const [search, setSearch] = useState('')
   const navigate = useNavigate()
+  const navigation = useNavigation()
+  const isLoading = navigation.state === 'loading'
 
   const handleForm = e => {
     e.preventDefault()
@@ -12,24 +15,34 @@ function Layout() {
 
   return (
     <div className="grid min-h-screen grid-rows-[auto_1fr] bg-stone-50 text-stone-950">
-      <header className="border-stone-150 flex flex-col items-center justify-center gap-2 border-b px-4 py-6">
-        <h1 className="text-xl font-bold">
-          <Link to="/" className="hover:underline">
-            Bazar Online
-          </Link>
-        </h1>
-        <form onSubmit={handleForm} className="flex flex-col">
-          <input
-            value={search}
-            onChange={e => setSearch(e.target.value)}
-            type="text"
-            className="bg-stone-200 px-8 py-2"
-            placeholder="Laptops, smartphones..."
-          />
-        </form>
+      <header className="border-stone-150 border-b px-16 py-6 md:px-4">
+        <div className="mx-auto flex max-w-4xl flex-col items-center gap-4 md:flex-row md:justify-between">
+          <h1 className="text-4xl font-bold">
+            <Link to="/" className="hover:underline">
+              Bazar Online
+            </Link>
+          </h1>
+          <form
+            onSubmit={handleForm}
+            className="relative flex flex-grow self-stretch md:max-w-lg"
+          >
+            <input
+              value={search}
+              onChange={e => setSearch(e.target.value)}
+              type="text"
+              className="w-full rounded-xl bg-stone-200 px-4 py-2 text-lg transition-[box-shadow] duration-300 focus:outline-none focus:ring-2 focus:ring-stone-400 md:px-8 md:py-4 md:text-xl"
+              placeholder="Laptops, smartphones..."
+            />
+            <input
+              type="submit"
+              value="Buscar"
+              className="absolute right-0 top-0 h-full cursor-pointer self-center rounded-r-xl bg-pink-300 px-4 py-2 text-lg font-semibold text-pink-950 transition-all duration-300 hover:bg-pink-400 focus:outline-none focus:ring-2 focus:ring-pink-500 md:px-8 md:py-3 md:text-2xl"
+            />
+          </form>
+        </div>
       </header>
       <main className="mx-auto max-w-4xl px-8 py-8">
-        <Outlet />
+        {isLoading ? <Spinner /> : <Outlet />}
       </main>
     </div>
   )
--- pruebas/02-bazar-universal/carlosmndzg/frontend/src/pages/HomePage.jsx
+++ pruebas/02-bazar-universal/carlosmndzg/frontend/src/pages/HomePage.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useNavigation } from 'react-router-dom'
+import Spinner from '../components/Spinner'
 
 function HomePage() {
   const [search, setSearch] = useState('')
   const navigate = useNavigate()
+  const navigation = useNavigation()
+  const isLoading = navigation.state === 'loading'
 
   const handleForm = e => {
     e.preventDefault()
@@ -16,6 +19,10 @@ function HomePage() {
     }
   }
 
+  if (isLoading) {
+    return <Spinner />
+  }
+
   return (
     <div className="h-[100dvh] bg-stone-50 px-12 text-stone-950">
       <div className="mx-auto flex h-full max-w-xl flex-col items-center justify-center">
@@ -25,13 +32,13 @@ function HomePage() {
             value={search}
             onChange={e => setSearch(e.target.value)}
             type="text"
-            className="w-full rounded-xl bg-stone-200 px-8 py-4 text-xl transition-[box-shadow] duration-300 focus:outline-none focus:ring focus:ring-stone-400"
+            className="w-full rounded-xl bg-stone-200 px-8 py-4 text-xl transition-[box-shadow] duration-300 focus:outline-none focus:ring-2 focus:ring-stone-400"
             placeholder="Laptops, smartphones..."
           />
           <input
             type="submit"
             value="Buscar"
-            className="absolute right-0 top-0 h-full cursor-pointer self-center rounded-r-xl bg-pink-300 px-8 py-3 text-2xl font-semibold text-pink-950 transition-colors hover:bg-pink-400 focus:outline-none focus:ring focus:ring-pink-500"
+            className="absolute right-0 top-0 h-full cursor-pointer self-center rounded-r-xl bg-pink-300 px-8 py-3 text-2xl font-semibold text-pink-950 transition-all duration-300 hover:bg-pink-400 focus:outline-none focus:ring-2 focus:ring-pink-500"
           />
         </form>
       </div>
